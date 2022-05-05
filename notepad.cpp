ed23d0f2c1edc2ef4523f3e9957f0ffa23815317
Agajan Nazarow <agajannazarow1999@gmail.com>
2022-05-06 01:43:46 +0300

    new document creation completed.

--- notepad.cpp
+++ notepad.cpp
@@ -13,30 +13,31 @@ Notepad::~Notepad()
     delete ui;
 }
 
+void Notepad::on_actionNew_triggered()
+{
+    currentFile.clear();
+    ui->textEdit->setText(QString());
+}
 
 void Notepad::on_actionOpen_triggered()
 {
 
 }
 
-
 void Notepad::on_actionSave_triggered()
 {
 
 }
-
 
 void Notepad::on_actionSave_As_triggered()
 {
 
 }
 
-
 void Notepad::on_actionPrint_triggered()
 {
 
 }
-
 
 void Notepad::on_actionExit_triggered()
 {
