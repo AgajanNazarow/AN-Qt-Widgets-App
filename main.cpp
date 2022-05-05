28f51933b84d83a86c0e58aad8b5540a98acd7e3
Agajan Nazarow <agajannazarow1999@gmail.com>
2022-05-06 01:12:12 +0300

    Renamed objects, added QTextEdit

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    Notepad w;
-    w.show();
-    return a.exec();
+    QApplication EditorApp(argc, argv);
+    Notepad editor;
+    editor.show();
+    return EditorApp.exec();
 }
